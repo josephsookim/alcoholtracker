aebd79a467be9d051f2bca1ecc6384f22ea00554
Joseph Kim <josek16@uci.edu>
2024-08-09 15:37:39 -0700

    :sparkles: Added LoginHeader component to the main page.

--- alc-tracker/src/app/layout.tsx
+++ alc-tracker/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import LoginHeader from '@/app/components/layout/LoginHeader';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* Header w/ Login Button */}
+        <LoginHeader/>
+        
+        {children}
+      </body>
     </html>
   );
 }
