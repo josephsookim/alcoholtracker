b50bc5508619aebdf3ee87c698e51d2e2bbb4179
Joseph Kim <josek16@uci.edu>
2024-08-09 21:03:02 -0700

    :sparkles: Add hero

--- alc-tracker/src/app/page.tsx
+++ alc-tracker/src/app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/app/components/layout/Hero";
 import Image from "next/image";
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
+      <Hero/>
     </div>
   );
 }
