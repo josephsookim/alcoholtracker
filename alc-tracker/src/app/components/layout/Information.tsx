b26eb70f3f7654fa626eb92768fc33b55c9d7f2d
Joseph Kim <josek16@uci.edu>
2024-08-09 22:16:54 -0700

    :sparkles: Update Information styling

--- alc-tracker/src/app/components/layout/Information.tsx
+++ alc-tracker/src/app/components/layout/Information.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 const Information = () => {
     return (
         <div className="flex justify-center items-center pt-2">
-            <Image src={"/information.svg"} alt="Information" width="1056" height="150" />
+            <Image src={"/information.svg"} alt="Information" width="1056" height="1116" />
         </div>
     )
 };
