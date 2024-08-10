f7106fac6d93e5fc726ab8cb53dc212b924fc88b
Joseph Kim <josek16@uci.edu>
2024-08-09 22:17:06 -0700

    :sparkles: Added Information section

--- alc-tracker/src/app/page.tsx
+++ alc-tracker/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/app/components/layout/Hero";
+import Information from "@/app/components/layout/Information";
 import Image from "next/image";
 import Link from 'next/link';
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <div>
       <Hero/>
+      <Information/>
     </div>
   );
 }
