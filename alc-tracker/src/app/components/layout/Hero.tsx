ed248c20178e0525df6b601e044dc954e7953a96
Joseph Kim <josek16@uci.edu>
2024-08-09 22:08:14 -0700

    :lipstick: Modify hero styling

--- alc-tracker/src/app/components/layout/Hero.tsx
+++ alc-tracker/src/app/components/layout/Hero.tsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 
 const Hero = () => {
     return (
-        <div className="flex justify-center items-center pt-8">
-            <Image src={"/hero-text.svg"} alt="Drink Responsibly - Your Future Matters" width="300" height="300" />
+        <div className="flex justify-center items-center pt-24">
+            <Image src={"/hero-text.svg"} alt="Drink Responsibly - Your Future Matters" width="607" height="150" />
         </div>
     )
 };
