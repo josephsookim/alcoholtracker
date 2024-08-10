6c1ebf3364836e7b9780fbfde4a14d87836f0741
Joseph Kim <josek16@uci.edu>
2024-08-10 10:19:54 -0700

    :sparkles: Add CTA to Home Page

--- alc-tracker/src/app/page.tsx
+++ alc-tracker/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/app/components/layout/Hero";
 import Information from "@/app/components/layout/Information";
+import CTA from "@/app/components/layout/CTA";
 import Image from "next/image";
 import Link from 'next/link';
 
@@ -8,6 +9,7 @@ export default function Home() {
     <div>
       <Hero/>
       <Information/>
+      <CTA/>
     </div>
   );
 }
