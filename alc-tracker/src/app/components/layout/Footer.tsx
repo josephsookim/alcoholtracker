80809884c0526abf5342754c38d7804d6da64bb5
Joseph Kim <josek16@uci.edu>
2024-08-13 09:10:31 -0700

    :pencil: Add name to footer

--- alc-tracker/src/app/components/layout/Footer.tsx
+++ alc-tracker/src/app/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer>
             <div className="container mx-auto flex justify-center items-center p-6 text-md text-white pt-20 pb-10">
-                Built with 🤍 by Joseph
+                Built with 🤍 by Joseph and Lester
             </div>
         </footer>
     );
