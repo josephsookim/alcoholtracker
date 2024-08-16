2c4a91e5b73ea97b9ffd0d329ae38cc4ab5c1940
Joseph Kim <josek16@uci.edu>
2024-08-16 15:51:34 -0700

    :sparkles: Add links to header

--- alc-tracker/src/app/components/layout/Header.tsx
+++ alc-tracker/src/app/components/layout/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
             <div className="container mx-auto flex items-center p-6 text-sm">
                 {/* Website Name */}
                 <div className="flex-grow text-white px-4">
-                    SipSmart                                                                                                            
+                    <a href="/">SipSmart</a>                                                                                                      
                 </div>
 
                 {/* Spacer */}
@@ -12,8 +12,8 @@ const Header = () => {
 
                 {/* NavBar */}
                 <ul className="flex space-x-4 px-4">
-                    <li className="text-white hover:underline cursor-pointer px-4">About</li>
-                    <li className="text-white hover:underline cursor-pointer px-4">Support</li>
+                    <li className="text-white hover:underline cursor-pointer px-4"><a href="/tracker">Track</a></li>
+                    <li className="text-white hover:underline cursor-pointer px-4"><a href="https://discord.gg/pCQyBd5zw9">Discord</a></li>
                 </ul>
             </div>
         </header>
