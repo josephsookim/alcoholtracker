1e91ca159c84fdfa5eeab32c00f085e68f6c392d
Joseph Kim <josek16@uci.edu>
2024-08-16 15:52:29 -0700

    :sparkles: Add link to CTA box

--- alc-tracker/src/app/components/layout/CTA.tsx
+++ alc-tracker/src/app/components/layout/CTA.tsx
@@ -4,7 +4,7 @@ const CTA = () => {
     return (
         <div className="flex justify-center items-center">
             <div className="hover:bg-[#313E9E] transition-colors duration-200">
-                <Image src={"/cta.svg"} alt="Get Started Now" width="607" height="117" />
+                <a href="/tracker"><Image src={"/cta.svg"} alt="Get Started Now" width="607" height="117" /></a>
             </div>
         </div>
     )
